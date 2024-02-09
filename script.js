10605ff9fd50cac583a39decbe6497d70018ba22
Gabriel Samuel <samuelgab246@gmail.com>
2024-02-09 04:15:12 -0800

    ES7

---
+++ script.js
@@ -0,0 +1,17 @@
+                    // includes
+
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+
+
+
+// Check if this array includes any name that has "John" inside of it. If it does, return that
+const dragon = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+dragon.filter(name => name.includes('John'))
+
+
+
+                // expennation operator
+
+const square = (X) => X**2;
+const cube = (S) => S**3;
